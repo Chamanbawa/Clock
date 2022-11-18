111e5ebe47110ce94fb76543dbbdc0095c628c99
Preet <chamanbawa12@gmail.com>
2022-11-18 16:04:40 -0600

    file connected

---
+++ asstes/script/index.js
@@ -0,0 +1,70 @@
+
+function onEvent(event, selector, callback) {
+
+    return selector.addEventListener(event, callback);
+
+}
+function getElement(selector, parent = document) {
+    return parent.getElementById(selector);
+}
+
+function select(selector, parent = document) {
+    return parent.querySelector(selector);
+
+}
+
+
+const time = select('.time');
+const input = select('.input');
+const set = select('.Set')
+const target = select('.target')
+
+const alarmSound = new Audio('./asstes/audio/win.mp3');
+alarmSound.type = 'audio/mp3';
+
+function bell() {
+    alarmSound.play();
+}
+
+setInterval(function () {
+    const currenTime = new Date();
+    let current = `${currenTime.getHours()}:${currenTime.getMinutes()}:${currenTime.getSeconds()}`;
+    time.innerText = current;
+
+    if ( current == input.value) {
+        console.log('hello');
+        bell();
+    }
+    
+    
+}, 1000);
+
+onEvent('click', set, setAlarm);
+function setAlarm(){
+    target.innerText =  `${input.value}`;
+
+
+}
+
+// function setAlarm() {
+
+//     setTime = new Date(input.value);
+//     alarmDate = setTime.getHours()
+//     console.log(`Setting alarm for ${alarmDate}....`);
+
+//     currenTime = new Date();
+
+//     let remtime = alarmDate - currenTime;
+
+//     console.log(remtime);
+
+//     if (remtime >= 0) {
+//         setTimeout(() => {
+//             bell();
+
+//         }, remtime)
+//     }
+// };
+
+   
+
